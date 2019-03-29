e83d7ad56698f8049f45c930c42a753dc0fefd0b
ArchieJude <tsevee_a1@denison.edu>
2019-03-29 13:22:44 -0400

    Test Number 5: Check if number of rows match the number of characters in a line.

--- kata word search/tests.cpp
+++ kata word search/tests.cpp
@@ -44,11 +44,11 @@ int fileOpen(const std::string& fileName) {
 }
 
 int testThree(const std::string& fileName) {
-/*
+	/*
 	Test Number 3:
 		Can it read the data file?
 
-*/
+	*/
 
 	if (fileExist(fileName) == 1 && fileOpen(fileName) == 1) {
 		fstream fileStream;
@@ -70,7 +70,7 @@ int testThree(const std::string& fileName) {
 
 int testFour(const std::string& fileName) {
 	/*
-		Test Number 4:
+	Test Number 4:
 		Check if the file is empty or not.
 	*/
 
@@ -93,13 +93,63 @@ int testFour(const std::string& fileName) {
 	}
 
 }
+
+int testFive(const std::string& fileName) {
+	/*
+	Test Number 5:
+		Check if number of rows match the number of characters in a line.
+	*/
+
+	fstream fileStream;
+	fileStream.open(fileName);
+	float lineCount = 0;
+	float avgCharCount = 0;
+	for (std::string line; std::getline(fileStream, line);) {
+		if (line.size() > 0) { // way to overcome newlines
+			
+			
+			if (lineCount >= 1) { //exclude the first line because it contains the words to be searched
+
+				float charCount = 0;
+				for (char c : line) {
+					if (c != ',') {
+						
+						charCount++;
+					}
+				}
+				avgCharCount = avgCharCount + charCount;
+			}
+
+			lineCount++;
+	
+		}
+	}
+
+	//decrement from lineCount because the line that contains the words to be searched does not count
+
+	lineCount--;
+	avgCharCount = avgCharCount / lineCount;
+	//check if the equivalency of lineCount and number of characters in a line 
+	//std::cout << lineCount << std::endl;
+	//std::cout << avgCharCount << std::endl;
+	if (lineCount == avgCharCount) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+	
+}
+
+
 int main() {
 	string fileName = "input.txt";
 	/*
 	std::cout << fileExist(fileName) << std::endl;
 	std::cout << fileOpen(fileName) << std::endl;
 	std::cout << testThree(fileName) << std::endl;
-	*/
-
 	std::cout << testFour(fileName) << std::endl;
+	*/
+	std::cout << testFive(fileName) << std::endl;
+	
 }
